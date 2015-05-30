6ab8e3005892f10cde83ede078330753f7f47ccf
Chieh-Hsun Yang <b99902049@gmail.com>
2015-05-30 15:28:31 +0800

    Re-implement hit function to solve problem of instability
    1. Encapsulate relay write function.
    2. Add delay between strike and back function.
    3. Rename delay variables.

--- CajonMaster/CajonMaster.ino
+++ CajonMaster/CajonMaster.ino
@@ -1,4 +1,6 @@
-const int t_gap = 100;
+const int strike_delay = 50;
+const int back_delay = 30;
+const int switch_delay = 60;
 const int relay_1 = 12;
 const int relay_2 = 13;
 char serial_input_buf[50];
@@ -8,6 +10,7 @@ void setup() {
   pinMode(relay_1, OUTPUT);
   pinMode(relay_2, OUTPUT);
   Serial.begin(9600);
+  relay_init();
   bpm = 70;
   print_bpm();
 }
@@ -28,7 +31,9 @@ void loop() {
   int period = 60000 / bpm;
   delay(period);  */
 
-  play("10010010", 120, 8);
+  //play("100100101001001010010010", 120, 8);
+  play("10110111", 120, 8);
+  delay(5000);
 }
 
 void play(String score, int bpm, int tempo) {
@@ -41,18 +46,50 @@ void play(String score, int bpm, int tempo) {
         break;
       case '1': // hit
         hit();
-        delay(period-t_gap);
+        delay(period - strike_delay - back_delay - switch_delay - switch_delay);
         break;
     }
   }
 }
 
 void hit() {
-  digitalWrite(relay_1, HIGH);
-  digitalWrite(relay_2, LOW);
-  delay(t_gap);
-  digitalWrite(relay_2, HIGH);
+  strike();
+  delay(strike_delay);
+  relay_init();
+  delay(switch_delay);
+  back();
+  delay(back_delay);
+  relay_init();
+  delay(switch_delay);
+}
+
+void strike() {
+  relay_1_high();
+}
+
+void back() {
+  relay_2_high();
+}
+
+void relay_init() {
   digitalWrite(relay_1, LOW);
+  digitalWrite(relay_2, LOW);
+}
+
+void relay_1_high() {
+  digitalWrite(relay_1, HIGH);
+}
+
+void relay_1_low() {
+  digitalWrite(relay_1, LOW);
+}
+
+void relay_2_high() {
+  digitalWrite(relay_2, HIGH);
+}
+
+void relay_2_low() {
+  digitalWrite(relay_2, LOW);
 }
 
 void print_bpm() {
